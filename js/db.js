137a71f53fda22d2635477c3fe75f52c70bc9b60
Jodannys Brito <jodannysaliendres@gmail.com>
2026-02-25 00:08:16 +0100

    modificado fi

--- js/db.js
+++ js/db.js
@@ -8,20 +8,26 @@ const DB = {
 
   async set(colName, id, data) {
     await fbSet(colName, id, { ...data, id });
-    delete this._cache[colName];
-    await this.getAll(colName);
+    if (this._cache[colName]) {
+      const idx = this._cache[colName].findIndex(x => x.id === id);
+      if (idx !== -1) this._cache[colName][idx] = { ...data, id };
+      else this._cache[colName].push({ ...data, id });
+    }
   },
 
   async update(colName, id, data) {
     await fbUpdate(colName, id, data);
-    delete this._cache[colName];
-    await this.getAll(colName);
+    if (this._cache[colName]) {
+      const idx = this._cache[colName].findIndex(x => x.id === id);
+      if (idx !== -1) this._cache[colName][idx] = { ...this._cache[colName][idx], ...data };
+    }
   },
 
   async delete(colName, id) {
     await fbDelete(colName, id);
-    delete this._cache[colName];
-    await this.getAll(colName);
+    if (this._cache[colName]) {
+      this._cache[colName] = this._cache[colName].filter(x => x.id !== id);
+    }
   },
 
   async query(colName, field, value) {
@@ -29,8 +35,14 @@ const DB = {
   },
 
   async init() {
-    await fbInit();
     await this.getAll('users');
+    const users = this._cache['users'] || [];
+    if (users.length === 0) {
+      console.warn('⚠️ No hay usuarios, creando admin inicial');
+      const adminInicial = { id: 'u1', nombre: 'Admin Principal', user: 'admin', pass: '1234', role: 'admin' };
+      await fbSet('users', 'u1', adminInicial);
+      this._cache['users'] = [adminInicial];
+    }
     await this.getAll('clientes');
     await this.getAll('creditos');
     await this.getAll('pagos');
