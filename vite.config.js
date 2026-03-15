c9d122299637bb6d778fc24e87943576c83a8ad0
Jodannys Brito <jodannysaliendres@gmail.com>
2026-03-16 00:57:25 +0100

    cliente vite

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 
 export default defineConfig({
   build: {
-    outDir: 'dist',
-    rollupOptions: {
-      // Cada archivo JS se mantiene como módulo separado
-      // preserveModules evita que Vite los fusione en un solo bundle
-      output: {
-        preserveModules: true,
-        preserveModulesRoot: 'js'
-      }
-    }
+    outDir: 'dist'
   }
 })
